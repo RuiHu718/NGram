fb7f8662d149e8216cb7766ecc3acd858255881f
Rui Hu <rui.hu6@icloud.com>
2017-04-09 17:28:39 +0800

    Initial commit

---
+++ ngrams.cpp
@@ -0,0 +1,24 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "map.h"
+#include "queue.h"
+#include "vector.h"
+#include "set.h"
+
+using namespace std;
+
+int main() {
+  // TODO: Finish the program!
+  Map<Set<string>, Vector<string> > test;
+
+  cout << "Exiting." << endl;
+  return 0;
+}
