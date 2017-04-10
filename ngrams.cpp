b6cf1464e6de216fb13ffbc63f9c88732763035c
Rui Hu <rui.hu6@icloud.com>
2017-04-10 15:33:29 +0800

    Seperates the map building code into a seperate function

--- ngrams.cpp
+++ ngrams.cpp
@@ -16,57 +16,70 @@
 
 using namespace std;
 
-int main() {
-  Map<Queue<string>, Vector<string> > test;
-  Queue<string> window;
+void buildMap(int n, ifstream &infile, Map<Queue<string>, Vector<string> > &nmap);
 
+int main() {
+  Map<Queue<string>, Vector<string> > nmap;
   ifstream infile;
   infile.open("test.txt");
+
+  buildMap(3, infile, nmap);
+
+  cout << "Exiting." << endl;
+  return 0;
+}
+
+
+/* Function: buildMap
+ * Usage:    buildMap(n, infile, nmap)
+ * -----------------------------------
+ * See assignment handout for detailed information
+ * Precondition
+ * Postcondition
+ */
+void buildMap(int n, ifstream &infile, Map<Queue<string>, Vector<string> > &nmap) {
+  Queue<string> window;
 
   TokenScanner scanner(infile);
   scanner.ignoreWhitespace();
   //need to add more punctuations here
   scanner.addWordCharacters(",");  
 
-  int i = 0;
+  int i = 1;
   while (scanner.hasMoreTokens()) {
     string token = scanner.nextToken();
-    if (i < 2) {
+    if (i < n) {
       window.enqueue(token);
       i++;
       continue;
     }
-    if (!test.containsKey(window)) {
+    if (!nmap.containsKey(window)) {
       Vector<string> suffixes;
       suffixes.add(token);
-      test.put(window, suffixes);
+      nmap.put(window, suffixes);
     } else {
-      cout << "the original vector: " << endl;
-      cout << test.get(window).toString() << endl;
-      cout << "the token is: " << token << endl;
-      cout << "adding to vector" << endl;
+      // cout << "the original vector: " << endl;
+      // cout << nmap.get(window).toString() << endl;
+      // cout << "the token is: " << token << endl;
+      // cout << "adding to vector" << endl;
 
       //why is the below syntax not working?
       //test.get(window).add(token);
-      Vector<string> t = test.get(window);
+      Vector<string> t = nmap.get(window);
       t.add(token);
-      test[window] = t;
-      cout << test.get(window).toString() << endl;
+      nmap[window] = t;
+      //cout << nmap.get(window).toString() << endl;
     }
 
-    string temp = window.dequeue();
+    string temp = window.dequeue(); // temp value is not used
     window.enqueue(token);
     //cout << token << endl;
   }  
 
   //print things out for testing
   //cout << test.toString() << endl;
-  for (Queue<string> k : test) {
+  for (Queue<string> k : nmap) {
     cout << k.toString() << "   ";
-    cout << test[k].toString() << endl;
+    cout << nmap[k].toString() << endl;
   }
-  
-  
-  cout << "Exiting." << endl;
-  return 0;
 }
